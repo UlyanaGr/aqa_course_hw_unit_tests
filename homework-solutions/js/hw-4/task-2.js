95d3ce2ee55a7ea6207019e8e217e12b0bcc737c
UlyanaGr <uliana.grechikha@gmail.com>
2025-08-02 16:02:00 +0300

    Resolved comments and added updates to the Task-2 and to the Task-3

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -19,27 +19,27 @@
   let maxAge = 60;
   let age;
 
-  age = 10;
+  // age = 10;
   // age = 17;
   // age = 18;
   // age = 19;
   // age = 59;
   // age = 60;
   // age = 61;
+  // age = "60"
+  // age = "test"
+
 
   if (typeof age !== "number") {
     console.log("Incorrect data type");
-  }
-
-  if (age < minAge) {
+  } else if (age < minAge) {
     console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
-  }
-  else if (age >= minAge && age < maxAge) {
+  } else if (age >= minAge && age < maxAge) {
     console.log("Welcome!");
-  }
-  else if (age > maxAge) {
+  } else if (age > maxAge) {
     console.log("Keep calm and look Culture channel");
-  }
-  else {
+  } else {
     console.log("Technical work");
   }
+
+
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -11,9 +11,7 @@ let minAge = 18;
 let maxAge = 60;
 let age;
 
-
-
-age = 10;
+// age = 10;
 // age = 17;
 // age = 18;
 // age = 19;
@@ -21,31 +19,23 @@ age = 10;
 // age = 60;
 // age = 61;
 // age = "25";
-// age = "test 19";
+// age = 'test 19';
 // age = "0";
 
 
-if (typeof age !== "number") {
-  console.log("Incorrect data type");
-}
-
-if (isNaN(age)) {
+if (!isNaN(age)) {
   age = Number(age);
+  if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+  } else if (age >= minAge && age < maxAge) {
+    console.log('Welcome!');
+  } else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log('Technical work');
+  }
+} else {
+  console.log('Incorrect data type');
 }
-  
-
-if (age < minAge) {
-  console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
-}
-else if (age >= minAge && age < maxAge) {
-  console.log("Welcome!");
-}
-else if (age > maxAge) {
-  console.log("Keep calm and look Culture channel");
-}
-else {
-  console.log("Technical work");
-}
-
 
 
