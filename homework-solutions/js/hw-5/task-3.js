0d3a8ec9ec5680704e3dcbf42c968bf970be68e0
UlyanaGr <uliana.grechikha@gmail.com>
2025-08-04 23:43:53 +0300

    Completed HW-5

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,7 +4,18 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
-
 let vowelsAndConsonantsResult = '';
+let vovels = 0;
+let consonants = 0;
+
+for (let i = 0; i < word.length; i++) {
+  if (word[i] != 'a' && word[i] != 'e' && word[i] != 'i' && word[i] != 'o' && word[i] != 'u') {
+    consonants++;
+  } else {
+    vovels++;
+  }
+}
+vowelsAndConsonantsResult = `${word} contains ${vovels} vowels and ${consonants} consonants`
+console.log(vowelsAndConsonantsResult);
 
 export { vowelsAndConsonantsResult };
