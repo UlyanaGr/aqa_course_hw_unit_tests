2c28c7f4ab6e0d79c3b44307bca1eaaa4b6129c3
UlyanaGr <uliana.grechikha@gmail.com>
2025-08-31 22:06:57 +0300

    Completed HW-JS-10

--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -7,8 +7,22 @@
   Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 */
 
-function countOccurrences(arr) {
-  // ваш код
+function countOccurrences(arr = []) {
+  // const obj = arr.reduce((acc, item, index) => {
+  //   acc[index] = item;
+  //   return acc;
+  // }, {});
+  // console.log(obj);
+
+  const result = {};
+
+  for (const num of arr) {
+    const currentCount = result[num] || 0;
+    result[num] = currentCount + 1;
+  }
+  return result;
 }
+
+console.log(countOccurrences([1, 2, 2, 3, 4, 4, 4, 5]));
 
 export { countOccurrences };
